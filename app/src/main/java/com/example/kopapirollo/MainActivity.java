912341a4f11edc69dedaa01fc7e86c1d538e10c6
SomaHam44 <77434852+SomaHam44@users.noreply.github.com>
2021-10-24 10:04:43 +0200

    Plusz feladatok

--- app/src/main/java/com/example/kopapirollo/MainActivity.java
+++ app/src/main/java/com/example/kopapirollo/MainActivity.java
@@ -14,14 +14,18 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-    private Button btnKo;
-    private Button btnPapir;
-    private Button btnOllo;
+    private ImageView btnKo;
+    private ImageView btnPapir;
+    private ImageView btnOllo;
     private ImageView tippKep, tippGepKep;
+    private ImageView[] felhasznaloEletek, gepEletek;
+    private ImageView gepElet1, gepElet2, gepElet3;
+    private ImageView felhasznaloElet1, felhasznaloElet2, felhasznaloElet3;
     private TextView eredmenyunk;
     private Random random;
     private Toast toast;
-    private int felhasznaloTippje, gepTippje,felhasznaloNyereseg, gepNyereseg, dontetlenKor;
+    private int felhasznaloTippje, gepTippje, felhasznaloNyereseg, gepNyereseg, dontetlenKor;
+    private int felhasznaloElet, gepElet;
     private AlertDialog.Builder alertBuild;
 
 
@@ -38,21 +42,22 @@ public class MainActivity extends AppCompatActivity {
                 if (gepTippje == 0) {
                     tippGepKep.setImageResource(R.drawable.rock);
                     dontetlenKor++;
-                    eredmenyunk.setText("Eredmény: Ember:  " + felhasznaloNyereseg
-                            + " Computer: " + gepNyereseg);
+                    eredmenyunk.setText("Döntetlenek száma: " + gepNyereseg);
                     Toast.makeText(getApplicationContext(), "Döntetlen", Toast.LENGTH_SHORT).show();
                 }
                 else if (gepTippje == 1) {
                     tippGepKep.setImageResource(R.drawable.paper);
                     gepNyereseg++;
-                    eredmenyunk.setText("Eredmény: Ember :" + felhasznaloNyereseg + " Computer: " + gepNyereseg);
+                    eletVesztesFelhasznalo();
+                    /*eredmenyunk.setText("Eredmény: Ember :" + felhasznaloNyereseg + " Computer: " + gepNyereseg);*/
                     Toast.makeText(getApplicationContext(), "Gép nyert", Toast.LENGTH_SHORT).show();
                 }
                 else {
                     tippGepKep.setImageResource(R.drawable.scissors);
                     felhasznaloNyereseg++;
-                    eredmenyunk.setText("Eredmény: Ember : " + felhasznaloNyereseg + " Computer: " +
-                    gepNyereseg);
+                    eletVesztesGep();
+                    /*eredmenyunk.setText("Eredmény: Ember : " + felhasznaloNyereseg + " Computer: " +
+                    gepNyereseg);*/
                     Toast.makeText(getApplicationContext(), "Felhasználó nyert", Toast.LENGTH_SHORT).show();
                 }
 
@@ -73,22 +78,23 @@ public class MainActivity extends AppCompatActivity {
                 if (gepTippje == 0) {
                     tippGepKep.setImageResource(R.drawable.rock);
                     felhasznaloNyereseg++;
-                    eredmenyunk.setText("Eredmény: Ember: " +  felhasznaloNyereseg
-                            +" Computer: " + gepNyereseg);
+                    eletVesztesGep();
+                    /*eredmenyunk.setText("Eredmény: Ember: " +  felhasznaloNyereseg
+                            +" Computer: " + gepNyereseg);*/
                     Toast.makeText(getApplicationContext(), "Felhasználó nyert", Toast.LENGTH_SHORT).show();
                 }
                 else if (gepTippje == 1) {
                     tippGepKep.setImageResource(R.drawable.paper);
                     dontetlenKor++;
-                    eredmenyunk.setText("Eredmény: Ember: " +  felhasznaloNyereseg +
-                            " Computer: " + gepNyereseg);
+                    eredmenyunk.setText("Döntetlenek száma: " + dontetlenKor);
                     Toast.makeText(getApplicationContext(), "Döntetlen", Toast.LENGTH_SHORT).show();
                 }
                 else {
                     tippGepKep.setImageResource(R.drawable.scissors);
                     gepNyereseg++;
-                    eredmenyunk.setText("Eredmény: Ember : " + felhasznaloNyereseg + " Computer: " +
-                            gepNyereseg);
+                    eletVesztesFelhasznalo();
+                    /*eredmenyunk.setText("Eredmény: Ember : " + felhasznaloNyereseg + " Computer: " +
+                            gepNyereseg);*/
                     Toast.makeText(getApplicationContext(), "Gép nyert", Toast.LENGTH_SHORT).show();
                 }
 
@@ -109,22 +115,23 @@ public class MainActivity extends AppCompatActivity {
                 if (gepTippje == 0) {
                     tippGepKep.setImageResource(R.drawable.rock);
                     gepNyereseg++;
-                    eredmenyunk.setText("Eredmény: Ember: " + felhasznaloNyereseg
-                    + " Computer: " + gepNyereseg);
+                    eletVesztesFelhasznalo();
+                    /*eredmenyunk.setText("Eredmény: Ember: " + felhasznaloNyereseg
+                    + " Computer: " + gepNyereseg);*/
                     Toast.makeText(getApplicationContext(), "Gép nyert", Toast.LENGTH_SHORT).show();
                 }
                 else if (gepTippje == 1) {
                     tippGepKep.setImageResource(R.drawable.paper);
                     felhasznaloNyereseg++;
-                    eredmenyunk.setText("Eredmény: Ember: "
-                            + felhasznaloNyereseg + " Computer: " + gepNyereseg);
+                    eletVesztesGep();
+                    /*eredmenyunk.setText("Eredmény: Ember: "
+                            + felhasznaloNyereseg + " Computer: " + gepNyereseg);*/
                     Toast.makeText(getApplicationContext(), "Felhasználó nyert", Toast.LENGTH_SHORT).show();
                 }
                 else {
                     tippGepKep.setImageResource(R.drawable.scissors);
                     dontetlenKor++;
-                    eredmenyunk.setText("Eredmény: Ember : " + felhasznaloNyereseg + " Computer: " +
-                            gepNyereseg);
+                    eredmenyunk.setText("Döntetlenek száma: " + dontetlenKor);
                     Toast.makeText(getApplicationContext(), "Döntetlen", Toast.LENGTH_SHORT).show();
                 }
 
@@ -148,15 +155,41 @@ public class MainActivity extends AppCompatActivity {
         toast = new Toast(getApplicationContext());
         alertBuild = new AlertDialog.Builder(this);
         alertDialogLetrehoz();
+        gepElet1 = findViewById(R.id.gepSziv1);
+        gepElet2 = findViewById(R.id.gepSziv2);
+        gepElet3 = findViewById(R.id.gepSziv3);
+        felhasznaloElet1 = findViewById(R.id.felhasznaloSziv1);
+        felhasznaloElet2 = findViewById(R.id.felhasznaloSziv2);
+        felhasznaloElet3 = findViewById(R.id.felhasznaloSziv3);
+        felhasznaloEletek = new ImageView[]{felhasznaloElet1, felhasznaloElet2, felhasznaloElet3};
+        gepEletek = new ImageView[]{gepElet1, gepElet2, gepElet3};
+        gepElet = 3;
+        felhasznaloElet = 3;
     }
 
     private void jatekVege() {
-        if (felhasznaloNyereseg > gepNyereseg) {
+        if (felhasznaloNyereseg > gepNyereseg && gepElet == 0) {
             alertBuild.setTitle("Győzelem").create().show();
         }
         else {
             alertBuild.setTitle("Vereség").create().show();
         }
+    }
+
+    private void eletVesztesFelhasznalo() {
+        if (felhasznaloElet > 0) {
+            felhasznaloElet--;
+
+        }
+        felhasznaloEletek[felhasznaloElet].setImageResource((R.drawable.heart1));
+    }
+
+    private void eletVesztesGep() {
+        if (gepElet > 0) {
+            gepElet--;
+
+        }
+        gepEletek[gepElet].setImageResource((R.drawable.heart1));
     }
 
     private void alertDialogLetrehoz() {
@@ -172,11 +205,20 @@ public class MainActivity extends AppCompatActivity {
         alertBuild.setPositiveButton("Igen", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
-                eredmenyunk.setText("Eredmény: Ember : 0 Computer: 0");
+                eredmenyunk.setText("Döntetlenek száma: 0");
                 tippKep.setImageResource(R.drawable.rock);
                 tippGepKep.setImageResource(R.drawable.rock);
                 felhasznaloNyereseg = 0;
                 gepNyereseg = 0;
+                dontetlenKor = 0;
+                gepElet = 3;
+                felhasznaloElet = 3;
+                felhasznaloElet1.setImageResource(R.drawable.heart2);
+                felhasznaloElet2.setImageResource(R.drawable.heart2);
+                felhasznaloElet3.setImageResource(R.drawable.heart2);
+                gepElet1.setImageResource(R.drawable.heart2);
+                gepElet2.setImageResource(R.drawable.heart2);
+                gepElet3.setImageResource(R.drawable.heart2);
                 closeContextMenu();
 
             }
